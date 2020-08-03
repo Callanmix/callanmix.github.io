5a8abd60a0d616f71b8be05be314cc8057a118f4
Callanmix <44954692+Callanmix@users.noreply.github.com>
2020-08-03 17:31:09 -0600

    added city finder app

--- js/footer.js
+++ js/footer.js
@@ -2,13 +2,10 @@ document.write(`
 <!-- Footer-->
 <footer class="footer bg-black small text-center text-white-50">
 <div class="social d-flex justify-content-center" style="font-size: 2.rem";>
-    <a class="mx-2" href="https://www.linkedin.com/in/callan-mix/" target="_blank"><i class="fab fa-linkedin-in fa-2x"></i></a>
-    <a class="mx-2" href="https://github.com/Callanmix" target="_blank"><i class="fab fa-github fa-2x"></i></a>
+    <a class="mx-2" href="https://www.linkedin.com/in/callan-mix/" target="_blank"><i class="fab fa-linkedin-in fa-5x"></i></a>
+    <a class="mx-2" href="https://github.com/Callanmix" target="_blank"><i class="fab fa-github fa-5x"></i></a>
 </div>
 <br>
-<div class="container">
-    Copyright © Callan Mix 2020
-</div>
 </footer>
 `)
     
--- js/nav_bar.js
+++ js/nav_bar.js
@@ -4,7 +4,6 @@ document.write(`
         <a class="navbar-brand js-scroll-trigger" href="index.html">Home</a><button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">Menu<i class="fas fa-bars"></i></button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
                 <li class="nav-item"><a class="nav-link js-scroll-trigger" href="projects.html">Projects</a></li>
                 <li class="nav-item"><a class="nav-link js-scroll-trigger" href="resume.html">Resume</a></li>
                 <li class="nav-item"><a class="nav-link js-scroll-trigger" href="contact.html">Contact</a></li>
